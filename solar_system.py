4d8fdb511258b46454fd32f966021cca9d4f2849
Amena Faruqi <afaruqi.97@gmail.com>
2020-02-13 20:46:25 +0000

    Add 2D plot of solar system

--- solar_system.py
+++ solar_system.py
@@ -1,10 +1,12 @@
 import astropy.constants as const
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
+from matplotlib import cm
 
-# ============================================================================
-# -------------------------------- CONSTANTS ---------------------------------
-# ============================================================================
+# ================================================================================================
+# ----------------------------------------- CONSTANTS --------------------------------------------
+# ================================================================================================
 
 G = const.G.value                  # Gravitational constant [m3/(kg s2)]
 atm = const.atm.value              # Earth's atmospheric pressure [Pa]
@@ -33,7 +35,7 @@ R_mars = 3389500                   # Radius of Mars [m]
 R_jupiter = 69911000               # Radius of Jupiter [m]
 R_saturn = 58232000                # Radius of Saturn [m]
 R_uranus = 25262000                # Radius of Uranus [m]
-R_neptune = 246220000              # Radius of Neptune [m]
+R_neptune = 24622000               # Radius of Neptune [m]
 
 a_mercury = 0.12                   # Albedo of Mercury
 a_venus = 0.59                     # Albedo of Venus
@@ -54,20 +56,22 @@ d_uranus = 19.14*au                # Orbital radius of Uranus [m]
 d_neptune = 30.2*au                # Orbital radius of Neptune [m]
 
 
-# ============================================================================
-# ------------------------------ ASSUMPTIONS ---------------------------------
-# ============================================================================
+# ================================================================================================
+# --------------------------------------- ASSUMPTIONS --------------------------------------------
+# ================================================================================================
 
 # 1) Perfectly circular, constant velocity orbits
 # 2) Uniform density spheres
 # 3) No planetary atmospheres
 
-# ============================================================================
-# ------------------------------ SSO CLASSES ---------------------------------
-#=============================================================================
+# ================================================================================================
+# ---------------------------------------- SSO CLASSES -------------------------------------------
+# ================================================================================================
+
 
 class SSO:
-    def __init__(self, radius, mass):
+    def __init__(self, name, radius, mass):
+        self.name = name
         self.radius = radius
         self.mass = mass
         self.volume = (4*np.pi/3)*self.radius**3
@@ -77,8 +81,8 @@ class SSO:
         pass               # CODE FOR MATHMETICAL ORBITAL TRAJECTORY HERE
 
 class Planet(SSO):
-    def __init__(self, radius, mass, d_orb, parent, albedo, atmosphere=False):
-        SSO.__init__(self, radius, mass)
+    def __init__(self, name, radius, mass, d_orb, parent, albedo, atmosphere=False):
+        SSO.__init__(self, name, radius, mass)
         self.d_orb = d_orb
         self.parent = parent
         self.albedo = albedo
@@ -94,15 +98,15 @@ class Planet(SSO):
         # ATMOSPHERIC PRESSURE CALCULATION?? 
       
 class Star(SSO):
-    def __init__(self, radius, mass, luminosity):
-        SSO.__init__(self, radius, mass)
+    def __init__(self, name, radius, mass, luminosity):
+        SSO.__init__(self, name, radius, mass)
         self.luminosity = luminosity
         self.temperature = (self.luminosity/(4*np.pi*sigma_sb*self.radius**2))**0.25
 
 
 class Moon(SSO):
-    def __init__(self, radius, mass, d_orb, parent, albedo):
-        SSO.__init__(self, radius, mass)
+    def __init__(self, name, radius, mass, d_orb, parent, albedo):
+        SSO.__init__(self, name, radius, mass)
         self.d_orb = d_orb
         self.parent = parent
         self.albedo = albedo
@@ -110,17 +114,48 @@ class Moon(SSO):
         self.radial_velocity = (2*np.pi*self.d_orb)/self.period
     
     
-# ============================================================================
-# ------------------------- GENERATE A SOLAR SYSTEM --------------------------
-# ============================================================================
+# ================================================================================================
+# --------------------------------- GENERATE A SOLAR SYSTEM --------------------------------------
+# ================================================================================================
         
-def make_solar_system():
-    sun = Star(R_sun, M_sun, L_sun)
-    mercury = Planet(R_mercury, M_mercury, d_mercury, sun, a_mercury)
-    venus  = Planet(R_venus, M_venus, d_venus, sun, a_venus)
-    earth = Planet(R_earth, M_earth, d_earth, sun, a_earth)
-    jupiter = Planet(R_jupiter, M_jupiter, d_jupiter, sun, a_jupiter)
-    saturn = Planet(R_saturn, M_saturn, d_saturn, sun, a_saturn)
-    uranus = Planet(R_uranus, M_uranus, d_uranus, sun, a_uranus)
-    neptune = Planet(R_neptune, M_neptune, d_neptune, sun, a_neptune)
+sun = Star('Sun', R_sun, M_sun, L_sun)
+mercury = Planet('Mercury', R_mercury, M_mercury, d_mercury, sun, a_mercury)
+venus  = Planet('Venus', R_venus, M_venus, d_venus, sun, a_venus)
+earth = Planet('Earth', R_earth, M_earth, d_earth, sun, a_earth)
+mars = Planet('Mars', R_mars, M_mars, d_mars, sun, a_mars)
+jupiter = Planet('Jupiter', R_jupiter, M_jupiter, d_jupiter, sun, a_jupiter)
+saturn = Planet('Saturn', R_saturn, M_saturn, d_saturn, sun, a_saturn)
+uranus = Planet('Uranus', R_uranus, M_uranus, d_uranus, sun, a_uranus)
+neptune = Planet('Neptune', R_neptune, M_neptune, d_neptune, sun, a_neptune)
+
+SSOs = [sun, mercury, venus, earth, mars, jupiter, saturn, uranus, neptune]
+
+
+def make_static_plot(SSOs):
+    ax = plt.subplot(111)
+    ax.set_ylim(-15,15)
+    colours = cm.rainbow(np.linspace(0, 1, len(SSOs)))
+    
+    for i in range(len(SSOs)):
+        radius = np.log10(SSOs[i].radius/1e6)
+        y = 0
+        if isinstance(SSOs[i], Planet):
+            x = SSOs[i].d_orb/(0.1*au)
+        else:
+            x = 0
+        obj_plot = Circle((x,y), radius, label = SSOs[i].name, color=colours[i])
+        ax.add_patch(obj_plot)
+    
+    plt.legend(bbox_to_anchor=(1.1,1))
+    ax.set_facecolor('black')
+    ax.set_xlabel('Distance from Sun (10 AU)')
+    plt.title('The Solar System (2D, static)')
+    ax.set_yticklabels([])
+    fig = plt.gcf()
+    fig.set_size_inches(25,3)
+    plt.autoscale(axis='x')
+    plt.show()
+        
+    
+
     
